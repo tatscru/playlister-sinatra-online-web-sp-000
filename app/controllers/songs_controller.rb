69789e8496ff1d4a060df80d989d83e1268c7648
tatiana.mojica <tatiana.mojica@bustle.com>
2019-06-15 02:26:08 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,5 @@
 class SongsController < Sinatra::Base
   enable :sessions
-  use Rack::Flash
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
