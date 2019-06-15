ac922c671c18efc3e016a8c1510b4afeb8c824db
tatiana.mojica <tatiana.mojica@bustle.com>
2019-06-15 02:27:02 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,6 +2,4 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
   has_many :song_genres
   has_many :songs, through: :song_genres
-  extend Slugifiable::ClassMethods
-  include Slugifiable::InstanceMethods
 end
